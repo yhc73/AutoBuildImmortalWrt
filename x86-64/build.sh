862bbe9fdfb1fe0f05134a6fa2f082f92ca64b22
yhc73 <57037342+yhc73@users.noreply.github.com>
2025-03-22 21:31:54 +0800

    Update build.sh

--- x86-64/build.sh
+++ x86-64/build.sh
@@ -31,8 +31,6 @@ PACKAGES="$PACKAGES luci-i18n-argon-config-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-opkg-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-ttyd-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-passwall-zh-cn"
-PACKAGES="$PACKAGES luci-app-openclash"
-PACKAGES="$PACKAGES luci-i18n-homeproxy-zh-cn"
 # 增加几个必备组件 方便用户安装iStore
 PACKAGES="$PACKAGES fdisk"
 PACKAGES="$PACKAGES script-utils"
