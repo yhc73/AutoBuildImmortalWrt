662b9f89aa2404f1b3714cee19c2c4bb6b9e157c
yhc73 <57037342+yhc73@users.noreply.github.com>
2025-03-22 10:28:08 +0800

    Update build.sh

--- x86-64/build.sh
+++ x86-64/build.sh
@@ -36,7 +36,6 @@ PACKAGES="$PACKAGES luci-i18n-homeproxy-zh-cn"
 # 增加几个必备组件 方便用户安装iStore
 PACKAGES="$PACKAGES fdisk"
 PACKAGES="$PACKAGES script-utils"
-PACKAGES="$PACKAGES luci-i18n-samba4-zh-cn"
 
 # 判断是否需要编译 Docker 插件
 if [ "$INCLUDE_DOCKER" = "yes" ]; then
