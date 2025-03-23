12f4e292af1318f67233afcccf288a4de30dc1f4
yhc73 <57037342+yhc73@users.noreply.github.com>
2025-03-23 14:40:01 +0800

    Update 99-custom.sh

--- files/etc/uci-defaults/99-custom.sh
+++ files/etc/uci-defaults/99-custom.sh
@@ -30,6 +30,23 @@ done
 # 删除多余空格
 ifnames=$(echo "$ifnames" | awk '{$1=$1};1')
 
+# 根据网卡数量配置网络
+count=0
+for iface in $(ls /sys/class/net | grep -v lo); do
+  # 检查是否有对应的设备，并且排除无线网卡
+  if [ -e /sys/class/net/$iface/device ] && [[ $iface == eth* || $iface == en* ]]; then
+    count=$((count + 1))
+  fi
+done
+if [ "$count" -eq 1 ]; then
+    # 单个网卡，设置为 DHCP 模式
+    uci set network.lan.proto='dhcp'
+    uci commit network
+elif [ "$count" -gt 1 ]; then
+    # 多个网卡，保持静态 IP
+    uci set network.lan.ipaddr='192.168.2.1'
+    uci commit network
+fi
    # 判断是否启用 PPPoE
    echo "print enable_pppoe value=== $enable_pppoe" >> $LOGFILE
    if [ "$enable_pppoe" = "yes" ]; then
