53fa4f16754093c656e7941b51f87ab17e9f9203
yhc73 <57037342+yhc73@users.noreply.github.com>
2025-03-31 07:28:05 +0800

    Update build.sh

--- rockchip/build.sh
+++ rockchip/build.sh
@@ -33,6 +33,7 @@ PACKAGES="$PACKAGES luci-app-argon-config"
 PACKAGES="$PACKAGES luci-i18n-argon-config-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-ttyd-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-passwall-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-homeproxy-zh-cn"
 # 增加几个必备组件 方便用户安装iStore
 PACKAGES="$PACKAGES fdisk"
 PACKAGES="$PACKAGES script-utils"
